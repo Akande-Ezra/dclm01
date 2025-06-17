6bf65c8c614df2d118a2f5cf9530ba5405cb1c21
akande Ezra <akandeezra240@gmail.com>
2025-06-17 19:56:44 +0100

    Fixed asset path for section.png in Forget.jsx

--- src/Components/Otp.jsx
+++ src/Components/Otp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Header_Logo from '/assets/logo 1.jpg';
-import Heroimage from '/assets/section.png';
+// import Heroimage from '/assets/section.png';
 import Featured_Icon from '/assets/Featured icon.png';
 import Arrow_Icon from '/assets/arrow-left.png';
 
